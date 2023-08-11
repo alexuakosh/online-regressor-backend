3fafc6a4741e309e8b88ef7a5208670c5426e7ad
avk.me <avk21.me@gmail.com>
2023-08-12 01:55:26 +0300

    deleted ann

--- ml_model.py
+++ ml_model.py
@@ -6,9 +6,7 @@ from sklearn import tree
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 import numpy as np
-import tensorflow as tf
-# 30.02, 59.19, 1021.00, 95.5
-#0.03237, 0, 2.18, 0, 0.458, 6.998, 45.8, 6.0622, 3, 222, 18.7, 394.63, 2.94
+
 
 class Regressors:
     def __init__(self, df):
@@ -44,7 +42,6 @@ class Regressors:
                     self.predictions[model.__class__.__name__] = model.predict([x_data])[0][0]
                 else:
                     self.predictions[model.__class__.__name__] = model.predict([x_data])[0]
-        self.ann(x_data)
         self.best_model['model'] = max(self.models)
         self.best_model['score'] = self.models[max(self.models)]
         self.best_model['prediction'] = round(self.predictions[max(self.models)], 2)
@@ -65,20 +62,3 @@ class Regressors:
             print(f'{model.__class__.__name__} Model Mean Squared Error: ', mean_squared_error(self.y_test, predicted_price))
             print()
 
-    def ann(self, data):
-        ann = tf.keras.models.Sequential()
-        ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
-        ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
-        ann.add(tf.keras.layers.Dense(units=1))
-        ann.compile(optimizer='adam', loss='mean_squared_error', metrics=[self.r_squared])
-        history = ann.fit(self.x_train, self.y_train, batch_size=32, epochs=100)
-        self.models['ANN'] = history.history['r_squared'][-1]
-        self.predictions['ANN'] = ann.predict([data]).tolist()[0][0]
-        print('//////////////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA///////')
-        print(self.predictions)
-
-    def r_squared(self, y_true, y_pred):
-        tss = tf.reduce_sum(tf.square(y_true - tf.reduce_mean(y_true)))
-        rss = tf.reduce_sum(tf.square(y_true - y_pred))
-        r_squared = 1 - (rss / tss)
-        return r_squared
